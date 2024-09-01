414c3c76821152ab2a031e5708114f294497f804
braydensilzell <144178170+braydensilzell@users.noreply.github.com>
2024-09-01 12:52:26 -0600

    Fix

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -100,7 +100,7 @@ public final class Constants {
         public static final class Mod0 { //TODO: This must be tuned to specific robot
             public static final int driveMotorID = 1; //right front drivee
             public static final int angleMotorID = 3; //right front steer
-            public static final int canCoderID = 2; //right front encoder
+            public static final int canCoderID = 2; //right front encoder/
             public static final Rotation2d angleOffset = Rotation2d.fromDegrees(3.86);
             public static final SwerveModuleConstants constants = 
                 new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
